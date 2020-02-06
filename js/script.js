6d1f950fbf9ffd04b6fb2eb4501916b5a6a74e50
iamsean <40333515@live.napier.ac.uk>
2020-02-06 14:34:31 +0000

    Completed FAQ section for large and medium screens

---
+++ js/script.js
@@ -0,0 +1,44 @@
+document.querySelector('.faq__plus--one').addEventListener('click', e => {
+
+   const answer = document.querySelector('.faq__content-answer--one');
+   const hide = 'hide';
+
+   answer.classList.toggle(hide);
+
+})
+
+document.querySelector('.faq__plus--two').addEventListener('click', e => {
+
+   const answer = document.querySelector('.faq__content-answer--two');
+   const hide = 'hide';
+
+   answer.classList.toggle(hide);
+
+})
+
+document.querySelector('.faq__plus--three').addEventListener('click', e => {
+
+   const answer = document.querySelector('.faq__content-answer--three');
+   const hide = 'hide';
+
+   answer.classList.toggle(hide);
+   
+})
+
+document.querySelector('.faq__plus--four').addEventListener('click', e => {
+
+   const answer = document.querySelector('.faq__content-answer--four');
+   const hide = 'hide';
+
+   answer.classList.toggle(hide);
+   
+})
+
+document.querySelector('.faq__plus--five').addEventListener('click', e => {
+
+   const answer = document.querySelector('.faq__content-answer--five');
+   const hide = 'hide';
+
+   answer.classList.toggle(hide);
+   
+})
